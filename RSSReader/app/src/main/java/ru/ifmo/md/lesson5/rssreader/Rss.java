cf64af7b07aa3ffbbe5ec7202f6e68024a7f912e
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2014-10-19 01:32:20 +0400

    add getters and setters

--- RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/Rss.java
+++ RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/Rss.java
@@ -7,10 +7,41 @@ public class Rss {
     private long mId;
     private String mName;
     private String mUrl;
-    private String mFavourite;
+    private int mFavourite;
 
     public Rss() {
         mId = -1;
     }
 
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public int getFavourite() {
+        return mFavourite;
+    }
+
+    public void setFavourite(int favourite) {
+        mFavourite = favourite;
+    }
 }
