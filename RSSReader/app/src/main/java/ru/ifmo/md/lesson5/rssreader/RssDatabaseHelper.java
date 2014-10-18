317483fb3b2293702e08a82057efe0cf0a9b529b
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2014-10-18 23:11:45 +0400

    add DBHelper

---
+++ RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/RssDatabaseHelper.java
@@ -0,0 +1,38 @@
+package ru.ifmo.md.lesson5.rssreader;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Nikita Yaschenko on 18.10.14.
+ */
+public class RssDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "rss.db";
+    private static final int VERSION = 1;
+
+    private static final String TABLE_RSS = "rss";
+
+
+    public RssDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_RSS + " (" +
+                "ID        INT  PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "NAME      TEXT                           NOT NULL," +
+                "URL       TEXT                           NOT NULL," +
+                "FAVOURITE INT" +
+                ");"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i2) {
+
+    }
+
+}
