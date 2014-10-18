37f6b88da954c2e72bec0d363f7645f807a9930d
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2014-10-18 23:23:46 +0400

    add Rss class

---
+++ RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/Rss.java
@@ -0,0 +1,16 @@
+package ru.ifmo.md.lesson5.rssreader;
+
+/**
+ * Created by Nikita Yaschenko on 18.10.14.
+ */
+public class Rss {
+    private long mId;
+    private String mName;
+    private String mUrl;
+    private String mFavourite;
+
+    public Rss() {
+        mId = -1;
+    }
+
+}
