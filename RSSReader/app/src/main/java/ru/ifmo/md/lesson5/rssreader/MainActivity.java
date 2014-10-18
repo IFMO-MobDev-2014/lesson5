2ce46162aff6e21f2066dd97403d6a03e422af5e
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2014-10-19 00:49:47 +0400

    add views initialization

--- RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/MainActivity.java
+++ RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/MainActivity.java
@@ -2,18 +2,52 @@ package ru.ifmo.md.lesson5.rssreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+    private Button mButtonAdd;
+    private EditText mEditTextAdd;
+    private ListView mListViewRss;
+
+    private RssManager mRssManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViews();
+        mRssManager = RssManager.get(this);
     }
 
+    private void setupViews() {
+        mButtonAdd = (Button) findViewById(R.id.button_addUrl);
+        mEditTextAdd = (EditText) findViewById(R.id.editText_addUrl);
+        mListViewRss = (ListView) findViewById(R.id.listView_rss);
+
+        mButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String potentialUrl = mEditTextAdd.getText().toString();
+                if (Patterns.WEB_URL.matcher(potentialUrl).matches()) {
+                    addRss(potentialUrl);
+                } else {
+
+                }
+            }
+        });
+    }
+
+    private void addRss(String url) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
