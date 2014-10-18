56158d1b5568a8082a70862960676679790b2c7d
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2014-10-19 01:31:34 +0400

    add 'insertRss' method

--- RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/RssManager.java
+++ RSSReader/app/src/main/java/ru/ifmo/md/lesson5/rssreader/RssManager.java
@@ -8,9 +8,11 @@ import android.content.Context;
 public class RssManager {
     private static RssManager sRssManager;
     private Context mAppContext;
+    private RssDatabaseHelper mHelper;
 
     private RssManager(Context context) {
         mAppContext = context;
+        mHelper = new RssDatabaseHelper(context);
     }
 
     public static RssManager get(Context context) {
@@ -19,4 +21,9 @@ public class RssManager {
         }
         return sRssManager;
     }
+
+    public void insertRss(Rss rss) {
+        mHelper.insertRss(rss);
+    }
+
 }
