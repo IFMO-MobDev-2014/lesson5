05ee53c922a413f68b9b5cf4744edfb9edacc3f6
shambala22 <asmavrin@rain.ifmo.ru>
2014-10-21 02:57:46 +0400

    it works with preinstalled BBC

--- app/src/main/java/ru/md/ifmo/lesson5/MyActivity.java
+++ app/src/main/java/ru/md/ifmo/lesson5/MyActivity.java
@@ -1,8 +1,9 @@
 package ru.md.ifmo.lesson5;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,37 +14,40 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
 
-public class MyActivity extends Activity implements AdapterView.OnItemClickListener {
+
+public class MyActivity extends Activity  {
 
     Button go;
     EditText editText;
     ListView listView;
     RSSTask loader;
+    AlertDialog.Builder dialog;
+    boolean browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MyActivity.this);
+        dialog.setTitle("Welcome!");
+        dialog.setMessage("Where do you want to show web pages?");
+        dialog.setPositiveButton("Browser", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                browser = true;
+            }
+        });
+        dialog.setNegativeButton("This app", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                browser = false;
+            }
+        });
+        dialog.show();
         go = (Button)findViewById(R.id.button2);
         editText = (EditText) findViewById(R.id.editText2);
         listView =(ListView) findViewById(R.id.listView2);
@@ -54,15 +58,8 @@ public class MyActivity extends Activity implements AdapterView.OnItemClickListe
             }
         });
 
-    }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
-                            long id) {
-        WebView link = (WebView) itemClicked.findViewById(R.id.link);
-        TextView linkHelper = (TextView) itemClicked.findViewById(R.id.link_helper);
-        link.setWebViewClient(new MyWebViewClient());
-        link.loadUrl(linkHelper.getText().toString());
+
     }
 
     private void parseXML() {
@@ -71,14 +68,12 @@ public class MyActivity extends Activity implements AdapterView.OnItemClickListe
             loader = new RSSTask();
             loader.execute(editText.getText().toString());
             if (loader.exception != null) {
-                Toast toast = Toast.makeText(getApplicationContext(), "fuck you", Toast.LENGTH_SHORT);
-                toast.show();
+                throw new Exception();
             }
 
             ArrayList<ItemMaster> itemsList = loader.get();
-            MyAdapter adapter = new MyAdapter(this, itemsList);
+            MyAdapter adapter = new MyAdapter(this, itemsList, browser);
             listView.setAdapter(adapter);
-            listView.setOnItemClickListener(this);
 
         } catch (Exception e) {
             Toast toast = Toast.makeText(getApplicationContext(), "Oops, something goes wrong", Toast.LENGTH_SHORT);
@@ -88,15 +83,6 @@ public class MyActivity extends Activity implements AdapterView.OnItemClickListe
 
     }
 
-    private class MyWebViewClient extends WebViewClient
-    {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url)
-        {
-            view.loadUrl(url);
-            return true;
-        }
-    }
 
 
 }
--- app/src/main/java/ru/md/ifmo/lesson5/MyAdapter.java
+++ app/src/main/java/ru/md/ifmo/lesson5/MyAdapter.java
@@ -8,42 +8,66 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MyAdapter extends ArrayAdapter<ItemMaster> {
     Context context;
     ArrayList<ItemMaster> items;
+    boolean browser;
 
-    public MyAdapter(Context context, ArrayList<ItemMaster> items) {
+    public MyAdapter(Context context, ArrayList<ItemMaster> items, boolean browser) {
         super(context, R.layout.list_element, items);
 
         this.context = context;
         this.items = items;
+        this.browser = browser;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        View elementView = inflater.inflate(R.layout.list_element, parent, false);
+        final View elementView = inflater.inflate(R.layout.list_element, parent, false);
         TextView title = (TextView) elementView.findViewById(R.id.title);
         TextView date = (TextView) elementView.findViewById(R.id.date);
         TextView description = (TextView) elementView.findViewById(R.id.description);
-        TextView linkHelper = (TextView) elementView.findViewById(R.id.link_helper);
 
-        linkHelper.setText(items.get(position).getLink());
+        Button button = (Button) elementView.findViewById((R.id.more));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebView link = (WebView) elementView.findViewById(R.id.link);
+                if (!browser) {
+                    link.setWebViewClient(new AppWebViewClient());
+                } else link.getSettings().setJavaScriptEnabled(true);
+                link.loadUrl(items.get(position).getLink());
+            }
+        });
+
+
         title.setText(items.get(position).getTitle());
         date.setText(items.get(position).getPubDate());
         description.setText(items.get(position).getDescription());
+
+
 
         return elementView;
 
     }
 
-
-
+    private class AppWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
+    }
 
 
 }
