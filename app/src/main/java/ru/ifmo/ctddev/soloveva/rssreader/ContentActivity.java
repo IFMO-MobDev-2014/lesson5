08fdf14658965ce243db57d305967c9893e8b5ed
s-sladen <m.luscinia@gmail.com>
2014-10-21 23:57:45 +0400

    new lines fix

--- app/src/main/java/ru/ifmo/ctddev/soloveva/rssreader/ContentActivity.java
+++ app/src/main/java/ru/ifmo/ctddev/soloveva/rssreader/ContentActivity.java
@@ -2,7 +2,11 @@ package ru.ifmo.ctddev.soloveva.rssreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 
 public class ContentActivity extends Activity {
@@ -13,7 +17,11 @@ public class ContentActivity extends Activity {
         super.onCreate(savedInstanceState);
         String html = getIntent().getStringExtra(HTML_KEY);
         WebView view = new WebView(this);
-        view.loadData(html, "text/html", "UTF-8");
+        try {
+            view.loadData(URLEncoder.encode(html, "UTF-8").replaceAll("\\+", "%20"), "text/html", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            Log.e("RSS", "something went wrong", e);
+        }
         setContentView(view);
     }
 }
