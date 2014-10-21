0442b5ff9eb5dd09a9d5f8289b277ec5e9d77278
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2014-10-21 23:43:44 +0400

    first commit

---
+++ app/src/main/java/com/example/kirill/rssreader/OnTaskCompleted.java
@@ -0,0 +1,11 @@
+package com.example.kirill.rssreader;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Kirill on 20.10.2014.
+ */
+public interface OnTaskCompleted {
+    void onTaskCompleted(ArrayList<FeedItem> result);
+}
