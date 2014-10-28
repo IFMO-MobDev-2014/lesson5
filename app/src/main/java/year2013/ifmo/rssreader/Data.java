3a9a713f71b43630d581a11621d8ba315892d1ff
Usacheva Julia <littlesun96@gmail.com>
2014-10-28 12:26:11 +0300

    rssreader

---
+++ app/src/main/java/year2013/ifmo/rssreader/Data.java
@@ -0,0 +1,10 @@
+package year2013.ifmo.rssreader;
+/*
+ * Created by Юлия on 25.10.2014.
+ */
+public class Data {
+    public String aThumbUrl;
+    public String aTitle;
+    public String aLink;
+    public String aDescription;
+}
